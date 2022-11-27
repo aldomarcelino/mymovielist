59af1d668dd5cde7bc455ffa821b79990c81baae
aldomarcelino <aldo115marcelino@gmail.com>
2022-11-27 11:10:04 +0700

    make movie list

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,9 +1,22 @@
-import React from "react";
+import { AiFillStar } from "react-icons/ai";
 
 export default function MovieCard({ movie }) {
   return (
-    <div className="">
-      <img src={movie} alt="korean" />
+    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[170px] inline-block cursor-pointer relative p-2">
+      <img
+        className="w-full h-auto block rounded-sm"
+        src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
+        alt={movie?.title}
+      />
+      <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 duration-300 opacity-0 hover:opacity-100 text-white m-1">
+        <p className="white-opacity-normal text-sm md:text-sm font-bold flex justify-center items-center h-full text-center">
+          {movie?.title}
+        </p>
+        <div className="flex flex-row gap-1 items-center absolute top-4 left-4 text-gray-300">
+          <AiFillStar />
+          {movie.vote_average}
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import Navbar from "../components/Navbar";
 import SearchPage from "../components/SearchPage";
-
-const movies = [
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-  "https://vuclipi-a.akamaihd.net/p/cloudinary/h_276,w_184,dpr_1.5,f_auto,c_thumb,q_auto:low/1166071189/d-1",
-];
+const key = process.env.REACT_APP_TMDBKEY;
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  console.log(key, "keyyyy");
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("Succes:", res.results);
+        setMovies(res.results);
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  }, []);
   return (
     <div>
       <Navbar />
       <SearchPage />
-      <div className=" mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="mt-10 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
+      <div className="mt-10 mx-auto sm:px-6 lg:max-w-7xl lg:px-12">
+        <h1 className="text-white font-extrabold text-3xl">
+          Original MyMovieList
+        </h1>
+        <div className="mt-5 grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-7 xl:grid-cols-7 xl:gap-x-8">
+          {movies?.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
           ))}
         </div>
       </div>
